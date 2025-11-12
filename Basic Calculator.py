3d8fb59c55d716c8169237bd084ba7e848c3cb5f
Manikandan.M <manisakthi694@gmail.com>
2025-11-12 16:21:11 +0530

    Add files via upload

---
+++ Basic Calculator.py
@@ -0,0 +1,37 @@
+Python 3.13.3 (tags/v3.13.3:6280bb5, Apr  8 2025, 14:47:33) [MSC v.1943 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> 
+= RESTART: C:/Users/Manikandan/OneDrive/Desktop/M.Mani Sakthi/Basic Calculator.py
+======== BASIC CALCULATOR ========
+Operations:
+1. Addition (+)
+2. Subtraction (-)
+3. Multiplication (*)
+4. Division (/)
+Type 'q' to quit.
+
+Enter your choice (1/2/3/4 or q): 1
+Enter first number: 2
+Enter second number: 4
+Result: 2.0 + 4.0 = 6.0
+
+Enter your choice (1/2/3/4 or q): 2
+Enter first number: 3
+Enter second number: 2
+Result: 3.0 - 2.0 = 1.0
+
+Enter your choice (1/2/3/4 or q): 3
+Enter first number: 2
+Enter second number: 4
+Result: 2.0 * 4.0 = 8.0
+
+Enter your choice (1/2/3/4 or q): 4
+Enter first number: 8
+Enter second number: 4
+Result: 8.0 / 4.0 = 2.0
+
+Enter your choice (1/2/3/4 or q): 1
+Enter first number: 5
+Enter second number: 8
+Result: 5.0 + 8.0 = 13.0
+
